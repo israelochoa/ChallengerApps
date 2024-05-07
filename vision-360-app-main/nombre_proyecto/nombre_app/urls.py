53fc2cb9a5f5902eb03d985531013fd119307df4
user <user@dpc.ddomain.org>
2024-05-07 13:49:13 -0400

    Commit 2

--- vision-360-app-main/nombre_proyecto/nombre_app/urls.py
+++ vision-360-app-main/nombre_proyecto/nombre_app/urls.py
@@ -3,7 +3,9 @@ from .views import inicio_view, login_view, homeView, bloques_campus_view
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
+    
     path('', inicio_view, name="page-comenzar"),
     path('login/', login_view, name="page-login"),
     path('inicio/', homeView, name="page-home"),
